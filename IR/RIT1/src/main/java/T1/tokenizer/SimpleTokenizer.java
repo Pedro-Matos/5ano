de56784fed441a2757fd3cf3551d177cfdb4c202
tiagoalexbastos <tiagoalexbastos@gmail.com>
2017-09-30 15:12:01 +0100

    stopwords and tokenizer

---
+++ IR/RIT1/src/main/java/T1/tokenizer/SimpleTokenizer.java
@@ -0,0 +1,9 @@
+package T1.tokenizer;
+
+import java.util.List;
+
+public class SimpleTokenizer implements Tokenizer {
+    public List<String> tokenize(String corpusText) {
+        return null;
+    }
+}
