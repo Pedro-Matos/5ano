d2e21d3a6d230bc950e402b64f50b60e704c7814
Tiago Bastos <tiagoalexbastos@gmail.com>
2017-10-26 11:49:17 +0100

    oi

---
+++ IM/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using GoogleMapsAPI.NET.API.Client;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String API_KEY = "AIzaSyAEGr6lbb8xTXsVu4VEsA-9N56Dh2VJ1NI";
+            IWebDriver driver = new ChromeDriver();
+            // Get client
+            var client = new MapsAPIClient(API_KEY);
+
+            // Geocoding an address
+            var geocodeResult = client.Geocoding.Geocode("1600 Amphitheatre Parkway, Mountain View, CA");
+
+            double latitude = 0.0;
+            double longitude = 0.0;
+            foreach (var money in geocodeResult.Results)
+            {
+                latitude = money.Geometry.Location.Latitude;
+                longitude = money.Geometry.Location.Longitude;
+            }
+
+            String lat = latitude.ToString().Replace(",", ".");
+            String lon = longitude.ToString().Replace(",", ".");
+
+            driver.Navigate().GoToUrl("https://www.google.com/maps/?q=-15.623037,18.388672");
+
+        }
+    }
+}
