312a897a05d55dc86c9b11f5c76c834b9c4e6ec1
Tiago Bastos <tiagoalexbastos@gmail.com>
2017-10-28 16:53:43 +0100

    nice selenium thank you bitch :)

---
+++ IM/IM_T1_GoogleMaps/IM_T1_GoogleMaps/Program.cs
@@ -0,0 +1,22 @@
+﻿using GoogleMapsAPI.NET.API.Client;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM_T1_GoogleMaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GoogleMapsWorker mapsWorker = new GoogleMapsWorker();
+            mapsWorker.searchDirections("Aveiro", "Porto");
+
+        }
+    }
+}
