491619920558fcc0449a247fce6e2cbceb7a5ebb
Pedro Matos <elitdruid@gmail.com>
2017-11-05 16:49:20 +0000

    more switch cases

--- IM/ModalidadeFramework/AppGui/AppGui/MainWindow.xaml.cs
+++ IM/ModalidadeFramework/AppGui/AppGui/MainWindow.xaml.cs
@@ -31,15 +31,101 @@ namespace AppGui
             var com = doc.Descendants("command").FirstOrDefault().Value;
             dynamic json = JsonConvert.DeserializeObject(com);
 
-            Shape _s = null;
-
             switch (json.CidadeDestino.ToString())
             {
                 case "Aveiro":
                     worker.goToPlace("Aveiro");
                     break;
+                case "Porto":
+                    worker.goToPlace("Porto");
+                        break;
+                case "Águeda":
+                    worker.goToPlace("Águeda");
+                    break;
+                case "Albergaria-a-Velha":
+                    worker.goToPlace("Albergaria-a-Velha");
+                    break;
+                case "Lisboa":
+                    worker.goToPlace("Lisboa");
+                    break;
+                case "Viseu":
+                    worker.goToPlace("Viseu");
+                    break;
+                case "Sever do Vouga":
+                    worker.goToPlace("Sever do Vouga");
+                    break;
+                case "Santa Comba Dão":
+                    worker.goToPlace("Santa Comba Dão");
+                        break;
+            }
+
+            switch(json.Zoom.ToString())
+            {
+                case "mais":
+                    worker.zoomIn();
+                    break;
+
+                case "menos":
+                    worker.zoomOut();
+                    break;
 
             }
+
+            switch(json.Opções.ToString())
+            {
+                case "painel":
+                    worker.togglePanel();
+                    break;
+                case "sem painel":
+                    worker.togglePanel();
+                    break;
+                case "sem direções":
+                    worker.closeDirections();
+                    break;
+                case "limpar pesquisa":
+                    worker.closeSearchBox();
+                    break;
+            }
+
+            switch(json.EscolherVista.ToString())
+            {
+                case "mapa":
+                    break;
+                case "satélite":
+                    worker.toggleSatellite();
+                    break;
+                case "terreno":
+                    worker.toggleTerreno();
+                    break;
+
+            }
+
+            switch(json.locaisEmCidades.ToString())
+            {
+                case "hospitais":
+                    worker.searchServiceAtLocation("hospitais", json.CidadeDestino.ToString());
+                    break;
+                case "interesse":
+                    worker.searchServiceAtLocation("interesse", json.CidadeDestino.ToString());
+                    break;
+                case "hoteis":
+                    worker.searchServiceAtLocation("hoteis", json.CidadeDestino.ToString());
+                    break;
+                case "museus":
+                    worker.searchServiceAtLocation("museus", json.CidadeDestino.ToString());
+                    break;
+                case "restaurantes":
+                    worker.searchServiceAtLocation("restaurantes", json.CidadeDestino.ToString());
+                    break;
+                case "aeroportos":
+                    worker.searchServiceAtLocation("aeroportos", json.CidadeDestino.ToString());
+                    break;
+
+
+
+
+            }
+
 
             // Dispatcher para ouvir outro reconhecimento
             //App.Current.Dispatcher.Invoke(() =>
