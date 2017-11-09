2c3aa7aa170afed9865e74677d02edda8a0b9bd1
tiagoalexbastos <tiagoalexbastos@gmail.com>
2017-11-09 11:38:58 +0000

    tts a dar

--- IM/ModalidadeFramework/AppGui/AppGui/Tts.cs
+++ IM/ModalidadeFramework/AppGui/AppGui/Tts.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using System.Media;
 using Microsoft.Speech.Synthesis;
 using Microsoft.Speech.AudioFormat;
@@ -10,7 +9,7 @@ using Microsoft.Speech.AudioFormat;
 
 
 
-namespace multimodal
+namespace AppGui
 {
     class Tts
     {
